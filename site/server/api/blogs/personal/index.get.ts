11a497aab16f1a4e4f54baf77cba4e9ef3ee82ef
Gilbert Rabut Tsurwa <tsurwagilbert@gmail.com>
2025-11-20 12:01:36 -0500

    deleting unused server routes
    - will consider reinstating some of these in my own way
    - i am actually using my backend web service, so i'll debate on:
       - whether i want to still create nuxt server endpoints that fetches
    the data from my backend
       - or bypass this and fetch the backend data using my components
    directly
    - i will think about this and decide soon
    - things are coming along nicely
    - over

--- site/server/api/blogs/personal/index.get.ts
+++
@@ -1,7 +0,0 @@
-import Post from "~~/models/posts";
-import type { PostInt } from "~~/interfaces/post";
-
-export default defineEventHandler(async (): Promise<PostInt[]> => {
-    const posts = await Post.find();
-    return posts;
-});
