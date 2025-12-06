3b35fa1146387658b29a329cf430505377738361
Gilbert Rabut Tsurwa <tsurwagilbert@gmail.com>
2025-12-05 23:25:35 -0500

    deleting unused code & markup

--- site/app/components/shared/Navigation.vue
+++ site/app/components/shared/Navigation.vue
@@ -1,42 +1,11 @@
-<script setup lang="ts">
-const isLoggedIn: Ref<boolean> = ref(false);
-// const supabase = useSupabaseClient();
-// const user = useSupabaseUser();
-// const route = useRoute();
-// const router = useRouter();
-
-// const signOut = async () => {
-//   try {
-//     const { error } = await supabase.auth.signOut();
-//     if (error) throw new Error(error.message);
-//     if (route.path === "/blog/uncensored") router.push("/login");
-//   } catch (error) {
-//   }
-// }
-</script>
-
 <template>
   <nav class="navigation">
     <slot name="logo"></slot>
     <ul class="navigation__list">
       <slot name="links"></slot>
-      <!-- <li class="navigation__list--item">
-        <NuxtLink to="/contact">Contact Me</NuxtLink>
-      </li> -->
     </ul>
     <ul class="navigation__list">
       <slot name="account"></slot>
-      <!-- account username -->
-
-      <!-- <li v-if="user?.role === 'authenticated'" class="navigation__list--item">
-        <span @click="signOut" style="color: #f0f0f0;">Sign Out</span>
-      </li>
-      <li v-if="user?.role === 'authenticated'" class="navigation__list--item">
-        <span style="color: #f0f0f0;">{{ user?.user_metadata.user_name || user.email }}</span>
-      </li> -->
-      <!-- <li v-else class="navigation__list--item">
-        <NuxtLink to="/login">Sign In</NuxtLink>
-      </li> -->
     </ul>
   </nav>
 </template>
@@ -47,7 +16,7 @@ const isLoggedIn: Ref<boolean> = ref(false);
   justify-content: space-between;
   align-items: center;
   width: 100%;
-  //@todo - enquêter pourquoi ça ne marche pas sans "!important"
+  /* @todo - investigater pourquoi ça ne marche pas sans "!important" */
   padding: 1.5rem 4rem !important;
   background-color: $colour-primary;
   margin-bottom: 2.5rem;
@@ -66,8 +35,7 @@ const isLoggedIn: Ref<boolean> = ref(false);
   .logo {
     font-size: 2.5rem;
     font-weight: 500;
-    color: $text-grey-light;
-    // font-family: $Josefin;
+    color: $grey-light;
     letter-spacing: 1.5px;
   }
 
