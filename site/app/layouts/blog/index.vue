20d6bfbd79e2ab963fbd03559f379ecb30c60d9d
Gilbert Rabut Tsurwa <tsurwagilbert@gmail.com>
2025-12-05 23:35:54 -0500

    and the same for this layout component (removing unused code)

--- site/app/layouts/blog/index.vue
+++ site/app/layouts/blog/index.vue
@@ -1,17 +1,32 @@
-<script setup lang="ts">
-// const router = useRouter();
-// const user = useSupabaseUser();
-// onMounted(() => {
-//     watchEffect(async () => {
-//         if (!user.value && ) await navigateTo("/projects");
-//     });
-// })
-</script>
-
 <template>
+    <Navigation>
+        <template #logo>
+            <LogoX />
+        </template>
+        <template #links>
+            <li class="navigation__list--item">
+                <NuxtLink to="/">Home</NuxtLink>
+            </li>
+            <li class="navigation__list--item">
+                <NuxtLink to="/blog">Blogs</NuxtLink>
+            </li>
+            <li class="navigation__list--item">
+                <NuxtLink to="/blog/authours/gilbert-rabut-tsurwa">About Me</NuxtLink>
+            </li>
+            <DevOnly>
+                <li class="navigation__list--item">
+                <NuxtLink to="/blog/uncensored">Uncensored Posts</NuxtLink>
+                </li>
+            </DevOnly>
+            <li class="navigation__list--item">
+                <NuxtLink to="/shop" target="_blank" rel="noreferrer noopener">Shop</NuxtLink>
+            </li>
+        </template>
+    </Navigation>
     <div class="blogsettings">
         <slot />
     </div>
+    <Footer />
 </template>
 
 <style lang="scss">
