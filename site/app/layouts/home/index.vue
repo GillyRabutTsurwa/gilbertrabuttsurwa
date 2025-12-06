7904e77aa714731b1d38aaf02cb0b131b2b3eb02
Gilbert Rabut Tsurwa <tsurwagilbert@gmail.com>
2025-12-05 23:30:19 -0500

    created new layouts

---
+++ site/app/layouts/home/index.vue
@@ -0,0 +1,23 @@
+<script setup>
+import { useBreakpoints } from "@/composables/useBreakpoints";
+
+const { pixels, toggleElementOnResize } = useBreakpoints();
+const show = ref(null);
+
+
+onMounted(() => {
+    if (import.meta.client) {
+        toggleElementOnResize(767);
+        const body = document.querySelector("body");
+        show.value = pixels.value < body.clientWidth ? true : false;
+    }
+});
+</script>
+
+<template>
+    <HomeNav/>
+    <div class="corps">
+        <slot />
+    </div>
+    <Footer/>
+</template>
