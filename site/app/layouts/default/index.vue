afa866ba842cf6dee87322d2e57f5667e12bfa4d
Gilbert Rabut Tsurwa <tsurwagilbert@gmail.com>
2025-12-05 23:34:59 -0500

    removing unused code from layout

--- site/app/layouts/default/index.vue
+++ site/app/layouts/default/index.vue
@@ -1,26 +1,5 @@
-<script setup>
-import { useBreakpoints } from "@/composables/useBreakpoints";
-
-const { pixels, toggleElementOnResize } = useBreakpoints();
-const show = ref(null);
-
-
-onMounted(() => {
-    if (process.client) {
-        toggleElementOnResize(767);
-        const body = document.querySelector("body");
-        show.value = pixels.value < body.clientWidth ? true : false;
-    }
-});
-</script>
 <template>
     <div class="corps">
         <slot />
     </div>
 </template>
-
-<style lang="scss">
-.corps {
-    font-family: "Kulim Park", Arial, Helvetica, sans-serif !important;
-}
-</style>
